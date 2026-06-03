c06bc2e973edcfce4774132cec1e1a54dcba04ab
Kauã Fernandes <kaua.fernandestec@gmail.com>
2026-06-03 08:26:33 -0300

    Add files via upload

---
+++ aula_09/valida.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+    <h1>Validar CPF</h1>
+    <form method="POST">
+
+    <h2>CPF</h2>
+    <p>Digite os números do seu cpf:</p>
+
+    <input type="text" name="cpf">
+
+<body>
+    
+<?php
+if [$_POST] {
+    $numero
+}
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
